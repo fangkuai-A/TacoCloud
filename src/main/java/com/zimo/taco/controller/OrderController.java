3b13614e05737ad86cc01ae63cb7c587bc7adeec
Zimo <liga0223@foxmail.com>
2022-03-07 20:14:18 +0800

    新增订单控制器

---
+++ src/main/java/com/zimo/taco/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.zimo.taco.controller;
+
+import com.zimo.taco.pojo.TacoOrder;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * Create by zimo at 2022/3/7
+ */
+@Log4j2
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
